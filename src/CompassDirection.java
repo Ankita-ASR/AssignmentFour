b6b2aa37ee817cb8cabc032e5d5d28a209e6d4ea
Ankita <ankita7saha@gmail.com>
2024-03-25 09:44:06 +0600

    E4.11 compass direction

--- src/CompassDirection.java
+++ src/CompassDirection.java
@@ -3,31 +3,30 @@ public class CompassDirection {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the angle (in degrees): ");
+        System.out.print("Enter the angle (in degrees)[0,360]: ");
         int angle = scanner.nextInt();
 
         String direction = getCompassDirection(angle);
         System.out.println("Nearest compass direction: " + direction);
     }
+
     public static String getCompassDirection(int angle) {
-        if (angle >= 338 || angle < 23) {
+        if (angle == 0) {
             return "N";
-        } else if (angle >= 23 && angle < 68) {
+        } else if (angle >= 0 && angle < 90) {
             return "NE";
-        } else if (angle >= 68 && angle < 113) {
+        } else if (angle ==90) {
             return "E";
-        } else if (angle >= 113 && angle < 158) {
+        } else if (angle > 90 && angle < 180) {
             return "SE";
-        } else if (angle >= 158 && angle < 203) {
+        } else if (angle == 180 ) {
             return "S";
-        } else if (angle >= 203 && angle < 248) {
+        } else if (angle >= 180 && angle < 270) {
             return "SW";
-        } else if (angle >= 248 && angle < 293) {
+        } else if (angle ==270) {
             return "W";
-        } else {
-            return "NW";
         }
+        else
+        return "NW";
     }
 }
-
-
